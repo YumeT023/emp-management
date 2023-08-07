9995e2afc4899bc7869f558223b0db63d001cbd0
Yume <hei.hajatiana@gmail.com>
2023-08-07 22:27:44 +0300

    fix: display enough info on employee table

--- src/main/java/com/prog4/controller/conf/AuthenticationFilter.java
+++ src/main/java/com/prog4/controller/conf/AuthenticationFilter.java
@@ -24,8 +24,6 @@ public class AuthenticationFilter implements Filter {
     HttpServletRequest httpRequest = (HttpServletRequest) request;
     HttpSession session = httpRequest.getSession(false);
 
-    log.info("filtering here {} {}", ((HttpServletRequest) request).getMethod(), ((HttpServletRequest) request).getServletPath());
-
     String path = ((HttpServletRequest) request).getServletPath();
 
     if (!path.startsWith("/employees")) {
